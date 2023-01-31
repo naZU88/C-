36a63a5f70e40d74b31585c4f0d28420bd2ffd4e
Yana <greedygoga@gmail.com>
2023-01-31 18:26:45 +0300

    Сделано дз1

---
+++ Sem_01_HM_01/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число:");
+string numberStr1 = Console.ReadLine();
+int number1 = Convert.ToInt32(numberStr1);
+
+Console.WriteLine("Введите второе число:");
+string numberStr2 = Console.ReadLine();
+int number2 = Convert.ToInt32(numberStr2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} > числа {number2}");
+}
+if (number1 < number2)
+{
+    Console.WriteLine($"Число {number2} > числа {number1}");
+}
+if (number1 == number2)
+{
+    Console.WriteLine($"Числа {number2} и {number1} равны");
+}
