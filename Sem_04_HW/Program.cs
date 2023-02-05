02d1f607e45993f6f0aa911c5aaae55c6956beb7
Yana <greedygoga@gmail.com>
2023-02-05 14:38:24 +0300

    Добавлен 4 семинар

---
+++ Sem_04_HW/Program.cs
@@ -0,0 +1,95 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+/*
+int getArray = 0;
+int GetArray(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out getArray))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Неправильно введено число или введено не число. Попробуйте снова!");
+        }
+    }
+    return getArray;
+}
+
+int Array1 = GetArray("Введите первое число:");
+int Array2 = GetArray("Введите второе число:");
+double result = Math.Pow(Array1, Array2);
+   Console.Write(result);
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+/*
+int getArray = 0;
+int GetArray(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out getArray))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Неправильно введено число или введено не число. Попробуйте снова!");
+        }
+    }
+    return Math.Abs(getArray);
+}
+
+void Counting(int Array)
+{
+    int sum = 0;
+    while (Array>0)
+    { 
+        sum = sum + Array%10;
+        Array = Array/10;
+    }
+    Console.WriteLine(sum);
+}
+int Array = GetArray("Введите число:");
+Counting(Array);
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+/*
+void GetArray(int[] array)
+{
+for (int i = 0; i < 8; i++)
+{
+    Console.WriteLine("Введите новый элемент массива:");
+    int newNumber = Convert.ToInt32(Console.ReadLine());
+    array[i] = newNumber;
+}
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < 7; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[7]}]");
+}
+int[] array = new int[8];
+GetArray(array);
+PrintArray(array);
+*/
---
+++ Sem_04_Tasks/Program.cs
@@ -0,0 +1,151 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+/*
+int GetNumber(string message)
+{
+int result = 0;
+
+while(true)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result) && result > 1)
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не число. Повторите ввод");
+}
+}
+
+return result;
+}
+
+int GetSumm(int number)
+{
+int sum = 0;
+
+for (int i = 1; i <= number; i++)
+{
+sum += i;
+}
+
+return sum;
+}
+
+int number = GetNumber("Введите число больше 1");
+int sum = GetSumm(number);
+Console.WriteLine($"сумма чисел от 1 до {number} = {sum}");
+*/
+
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+/*
+int GetNumber(string message)
+{
+int result = 0;
+
+while (true)
+{
+Console.WriteLine(message);
+
+if (int.TryParse(Console.ReadLine(), out result))
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не число. Повторите ввод");
+}
+}
+
+return Math.Abs(result);
+}
+
+
+
+void GetLong(int GetNumber)
+{
+string GetText = GetNumber.ToString();
+int longText = GetText.count;
+Console.WriteLine(longText);
+}
+
+int number = GetNumber("Введите любое число");
+GetLong(number);
+*/
+
+
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+/*
+int GetNumber(string message)
+{
+int result = 0;
+
+while (true)
+{
+Console.WriteLine(message);
+
+if (int.TryParse(Console.ReadLine(), out result) && result >= 1)
+{
+break;
+}
+else
+{
+Console.WriteLine("Ввели не число. Повторите ввод");
+}
+}
+return result;
+}
+int MultipleNumers (int number)
+{
+int multi = 1;
+for (int i = 1; i <= number; i++)
+{
+multi = multi * i;
+}
+return multi;
+}
+int number = GetNumber("Введите число: ");
+int rezult = MultipleNumers(number);
+Console.WriteLine(rezult);
+*/
+
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+/*
+void newArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+void printArray(int[] array)
+{
+    int position = 0;
+    int count = array.Length;
+    while (position<count)
+    {
+     Console.WriteLine(array[position]);
+     position++;
+
+    }
+}
+
+int[] collection = new int[8];
+newArray(collection);
+printArray(collection);
+*/
+
